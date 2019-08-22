20aaa425ee9f9e131a1627d6a8530cd766d7b11d
Dawson Hessler <funkemunkybiz@gmail.com>
2019-08-22 12:26:48 -0400

    Removing all files

--- src/cc/funkemunky/fixer/api/event/ListenerManager.java
+++
@@ -1,9 +0,0 @@
-package cc.funkemunky.fixer.api.event;
-
-import cc.funkemunky.fixer.impl.listeners.MoveEvent;
-
-public class ListenerManager {
-    public ListenerManager() {
-        new MoveEvent();
-    }
-}
